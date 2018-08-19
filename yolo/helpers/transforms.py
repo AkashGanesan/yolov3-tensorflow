4ec3eb91220b738fc3bea454faf16818c3dd55d0
Akash Ganesan <akaberto@gmail.com>
2018-08-19 10:55:30 -0400

    Collected DarkNet transforms to a class.
    Additionally have inplace transforms as an option.

--- yolo/helpers/transforms.py
+++ yolo/helpers/transforms.py
@@ -2,12 +2,12 @@ from itertools import zip_longest
 from typing import Dict, Tuple, List, Any, NoReturn
 from typing import Mapping, Sequence, Union, Mapping, Iterable
 import ast
-
-def sepToList(s : str, sep : str) ->list:
-    return s.split(sep)
-
+from copy import deepcopy
 
 def tryCastNumeric(s : str) -> Union[int,float, str]:
+    """ 
+    Given a string, it converts it to an int or float if possible;
+    else returns the string as is"""
     if type(s) is str:
         s = s.strip()
         if s.isnumeric():
@@ -16,74 +16,90 @@ def tryCastNumeric(s : str) -> Union[int,float, str]:
             return float(s)
         else:
             return s
-    
-def strIsCommaSepList(s : str,
-                      sep=",") -> bool:
+
+
+        
+def strIsCommaSepList(s : str,  sep=",") -> bool:
+    """ Check if it the given string looks like a comma separated
+    list"""
     return sep in s
 
 
+
 def tryStrToList(s: str,
-                 sep = ","):    
+                 sep = ",") -> Union[str,List]:
+    """ If comma separated list-like string, return a list, else
+    return string as is"""
     if strIsCommaSepList(s):
         return s.split(sep)
     return s
 
 
-def applyCasts(s : Union[str,List,None]) -> Union[str,List[Union[str, int, float, None]]]:
-    if s is None:
-        return None
-    s = tryStrToList(s)
-    if (type(s) is str):
-        return tryCastNumeric(s)
-    elif type(s) is list:
-        return list(map(applyCasts,s))
-            
-def unsafeTransformDict(d : Dict) -> NoReturn:
-    mutate_dict(applyCasts, d)
-    mutate_dict(lambda x: list(grouper(x,2)), d, filterOnVal=trueIfEqual)
-            
-
-
-
-# def tryCastNumeric(s : Union[str, List, None]) -> Union[str,int]:
-#     if s is None:
-#         return None
-#     elif type(s) is str:
-#         s = s.strip()
-#         if s.isnumeric():
-#             return int(s.strip())
-#         elif "." in s:
-#             try: 
-#                 return float(s)
-#             except:
-#                 return s
-#     elif type(s) is list:
-#         return  list(map(tryCastNumeric,s))    
-#     else:
-#         return s
-
-def trueAlways(v):
+def trueAlways(v : Any) -> bool:
     return True
 
-def trueIfEqual(v, eqVal="anchors"):
+def trueIfEqual(v : Any, eqVal="anchors") -> bool:
+    """ Returns if v matches eqVal.  """
     return (v == eqVal)
+
 
 def mutate_dict(f : Mapping[Any, Any]
                 ,d : Dict
-                ,filterOnVal=trueAlways):
+                ,filterOnKey=trueAlways) -> NoReturn:
+    """ Aplly function over all values for keys satisfying filterOnKey """
     for k, v in d.items():
-        if v is not None and filterOnVal(k):
+        if v is not None and filterOnKey(k):
             d[k] = f(v)
 
 
             
 def grouper(iterable : Iterable,
             n : int,
-            fillvalue=None):
+            fillvalue=None) -> Iterable:
+    """ Groups items in iterable with optional fill with a given value"""
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)        
 
 
+
+class DarkNetTransformer:
+    """ DarkNet transformer for cleaning up the list of dictionaries to a saner formate"""
+    @classmethod
+    def applyCasts(cls, s : Union[str,List,None]) -> Union[str,List[Union[str, int, float, None]]]:
+        """ Applies numeric casts and comma sep list like strings to lists"""
+        if s is None:
+            return None
+        s = tryStrToList(s)
+        if (type(s) is str):
+            return tryCastNumeric(s)
+        elif type(s) is list:
+            return list(map(cls.applyCasts,s))
+
+    @classmethod        
+    def transformDict(cls, d : Dict, inplace=True) -> NoReturn:
+        """ Runs the darknet transformation over the dictionary"""
+        if (inplace == False):
+            d = deepcopy(d)
+        cls._unsafeTransformDict(d)
+        return d
+    
+    @classmethod        
+    def _unsafeTransformDict(cls, d : Dict) -> NoReturn:
+        mutate_dict(cls.applyCasts, d)
+        mutate_dict(lambda x: list(grouper(x,2)), d, filterOnKey=trueIfEqual)
+
+
+    @classmethod
+    def transform(cls, lst : List, inplace=True) -> List[Dict]:
+        if (inplace == False):
+            d = deepcopy(d)
+        cls._unsafeTransformDictLists(lst)
+            
+    @classmethod                
+    def _unsafeTransformDictLists(cls, lst : List) -> NoReturn:
+        list(map(cls.transformDict, lst))
+
+
         
 if __name__=="__main__":
-    print ("THis is sparta")
+    pass
